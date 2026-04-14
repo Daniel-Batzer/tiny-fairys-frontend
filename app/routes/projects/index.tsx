8aeb61813c243e1d547893f594a663c490dc75dc
DBatz <d.batzer@web.de>
2026-04-14 11:16:00 +0200

    added pagination

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,6 +1,8 @@
 import type { Project } from "~/types";
 import type { Route } from "./+types/index";
 import ProjectCard from "~/components/ProjectCard";
+import { useState } from "react";
+import Pagination from "~/components/Pagination";
 
 export async function loader({
   request,
@@ -13,14 +15,33 @@ export async function loader({
 const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
   const { projects } = loaderData as { projects: Project[] };
 
+  const [currentPage, setcurrentPage] = useState(1);
+  const projectsPerPage = 2;
+
+  // calculate total pages
+  const totalPages = Math.ceil(projects.length / projectsPerPage);
+
+  // Get current pages projects
+  const indexOfLastProject = currentPage * projectsPerPage;
+  const indexOfFirstProject = indexOfLastProject - projectsPerPage;
+  const currentProjects = projects.slice(
+    indexOfFirstProject,
+    indexOfLastProject,
+  );
+
   return (
     <>
       <h2 className="text-3xl font-bold mb-8 text-center">Projects</h2>
       <div className="grid gap-6 sm:grid-cols-2">
-        {projects.map((project) => (
+        {currentProjects.map((project) => (
           <ProjectCard key={project.id} project={project} />
         ))}
       </div>
+      <Pagination
+        totalPages={totalPages}
+        currentPage={currentPage}
+        onPageChange={setcurrentPage}
+      />
     </>
   );
 };
